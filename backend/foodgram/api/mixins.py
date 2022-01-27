b795b2b3c0210b2bb37a1b8e12d88652105f55b8
sergkuzora <sergkuzora@yandex.ru>
2022-01-27 05:05:50 +0300

    v1.3

--- backend/foodgram/api/mixins.py
+++ backend/foodgram/api/mixins.py
@@ -1,10 +1,11 @@
 from rest_framework import mixins, permissions, viewsets
+from sympy import Li
 
-from api.paginators import PageNumberPaginatorModified
+from api.paginators import LimitPageNumberPagination
 
 
 class CreateDestroyMixinView(
     mixins.CreateModelMixin, mixins.DestroyModelMixin, viewsets.GenericViewSet,
 ):
     permission_classes = (permissions.IsAuthenticated,)
-    pagination_class = PageNumberPaginatorModified
+    pagination_class = LimitPageNumberPagination
