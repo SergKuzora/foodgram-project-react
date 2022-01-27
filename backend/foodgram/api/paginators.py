4a0c9e89d1b045db46ba95d02aed0c9fb00d7f31
sergkuzora <sergkuzora@yandex.ru>
2022-01-27 04:50:56 +0300

    v1.3

--- backend/foodgram/api/paginators.py
+++ backend/foodgram/api/paginators.py
@@ -1,5 +1,6 @@
 from rest_framework.pagination import PageNumberPagination
 
 
-class PageNumberPaginatorModified(PageNumberPagination):
+class LimitPageNumberPagination(PageNumberPagination):
     page_size_query_param = 'limit'
+    page_size = 10
