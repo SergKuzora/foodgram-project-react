a05ebb33b1fa4e71b2742d268c67d30814171313
sergkuzora <sergkuzora@yandex.ru>
2022-01-29 19:41:28 +0300

    v3.4

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'nez6c#rlmzqx%5%828p2p(clp)g@u6!ga#t8b%%=_whqe#a*@9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
