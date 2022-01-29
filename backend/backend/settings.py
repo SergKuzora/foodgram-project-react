23fc6bce7977fb6441081c8f88aa38eb3086e368
sergkuzora <sergkuzora@yandex.ru>
2022-01-29 05:24:35 +0300

    v3.3

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -19,19 +19,19 @@ ALLOWED_HOSTS = ['*']
 # Application definition
 
 INSTALLED_APPS = [
-    # 'django.contrib.admin',
+    'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'users',
+    'recipes',
+    'api',
     'rest_framework',
     'rest_framework.authtoken',
     'djoser',
     'django_filters',
-    'recipes',
-    'api',
-    'users',
     'colorfield',
     'corsheaders',
 ]
